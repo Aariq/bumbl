8cb16856afc7f04ae10be6703a36d25d22ed8f1f
Aariq <scottericr@gmail.com>
2019-08-05 19:43:04 -0400

    added colony weights data

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Bumblebee colony weight data from Crone and Williams, 2016
+#'
+#' A dataset containing the weights of bumblebee colonies...
+#'
+#' @format A data frame with 178 rows and 10 columns
+#' \describe{
+#'   \item{Pair}{???}
+#'   \item{Site}{Which site/colony}
+#'   \item{ColonyID}{Which colony/site}
+#'   \item{TRT}{Early supplemented (ES) or late supplemented (LS)}
+#'   \item{Condition}{???}
+#'   \item{TrueColonyWt_g}{True colony weight, in grams}
+#'   \item{NectarWt_g}{The weight of nectar in the colonies, in grams}
+#'   \item{A_ColonyWt_g}{???}
+#'   \item{B_BoxWt_g}{The tare weight of the box the colony is in, in grams}
+#' }
+#' @source \url{link to archived data???}
+"colony_weights"
