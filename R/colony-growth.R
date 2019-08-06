97f1d9af56717a8d63b01094eaef3389bb88ce71
Aariq <scottericr@gmail.com>
2019-08-06 14:05:27 -0400

    implemented function to map brkpt over all colonies

--- R/colony-growth.R
+++ R/colony-growth.R
@@ -13,6 +13,9 @@
 #' @export
 #'
 #' @examples
+#' testbees <- colony_weights %>% filter(ColonyID == 68)
+#' mytaus <- (seq(2,8,0.1))
+#' brkpt(testbees, mytaus, Round, log(TrueColonyWt_g) ~ Round)
 brkpt <- function(data, taus, t, formula){
   #TODO: make sure none of the variables are called '.post'
   fterms <- terms(formula)
@@ -42,18 +45,54 @@ brkpt <- function(data, taus, t, formula){
 
     m0 = try(lm(f, data = data2))
     if(class(m0) != "try-error") LLs[i] = logLik(m0)
-    #what if there is an error?
+    #TODO: what if there is an error?
     # LLs
   }
   tau_win <- taus[which(LLs == max(LLs))]
-  #TODO: ensure that only one winner is chosen in the case of multiple equivalently good Tau's
-  # Possibly, if length(winner)> 1, then take median(winner) and re-fit model with that
 
+  # This should be documented in "Details":
+  # If there is more than one equivalent tau (all have same LL), then re-fit model with median of those taus
   if(length(tau_win) > 1){
     tau_win <- median(tau_win)
   }
-  # I don't really like that it re-fits the model.  I could have it save them all and only re-fit in the case of a tau tie.
+  #TODO: I don't really like that it re-fits the model.  I could have it save them all and only re-fit in the case of a tau tie.
   data_win <- mutate(data, .post = ifelse(!!t <= tau_win, 0, !!t - tau_win))
   m_win <- lm(f, data = data_win)
   return(tibble(tau = tau_win, model = list(m_win)))
+}
+
+
+
+#' Fit breakpoint growth models to many colonies.
+#' Fits breakpoint growth models to each colony in a dataset and returns the estimated breakpoint (tau) and some other things...
+#'
+#' @param data a dataframe or tibble
+#' @param colonyID the unquoted column name of the colony ID variable
+#' @param taus vector of taus to test
+#' @param t the unquoted column name of the time variable in (units???)
+#' @param formula a formula passed to `lm()`
+#'
+#' @return a dataframe
+#' @export
+#'
+#' @examples
+#' data(colony_weights)
+#' mytaus <- (seq(2,8,0.1))
+#' bumbl(colony_weights, ColonyID, mytaus, Round, log(TrueColonyWt_g) ~ Round)
+
+bumbl <- function(data, colonyID, taus, t, formula){
+  summary_data <-
+    data %>%
+    group_by({{colonyID}}) %>%
+    nest() %>%
+    mutate(model = map(data, ~brkpt(., taus, {{t}}, formula))) %>%
+    unnest(model) %>%
+    mutate(coefs = map(model, tidy)) %>%
+    unnest(coefs, .preserve = "model") %>%
+    select({{colonyID}}, tau, model, term, estimate) %>%
+    spread(key = term, value = estimate) %>%
+    mutate(logNmax = map_dbl(model, ~max(predict(.), na.rm = TRUE))) %>%
+    select(-model) %>%
+    select({{colonyID}}, tau, logNo = `(Intercept)`, loglam = {{t}}, decay = .post, everything())
+  full_join(data, summary_data)
 }
