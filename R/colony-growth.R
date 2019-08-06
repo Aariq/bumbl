4f60c59d94d2a58cae8724179f536ec0dbfe2fc7
Aariq <scottericr@gmail.com>
2019-08-06 11:18:07 -0400

    wrote more tests, ready to scale up I think

--- R/colony-growth.R
+++ R/colony-growth.R
@@ -1,5 +1,3 @@
-
-
 #' Fit breakpoint model to individual colony
 #' Fits models using a range of taus and picks the best one using maximum liklihood
 #'
@@ -11,26 +9,36 @@
 #' @return dataframe with two columns
 #'
 #' @import dplyr
+#' @import rlang
 #' @export
 #'
 #' @examples
 brkpt <- function(data, taus, t, formula){
   #TODO: make sure none of the variables are called '.post'
-  #TODO: don't bother if Tau's extend past last t.  Don't even fit those models.  Warn user maybe (verbose = TRUE??)
   fterms <- terms(formula)
   t <- enquo(t)
+
+  #Check that time variable is in the formula
   if(!quo_name(t) %in% attr(fterms, "term.labels")) {
     stop("'t=' should specify the time variable in the formula")
     }
+  #Check that at least some taus are in range of t
+  if(all(taus > max(data[[quo_name(t)]]))) {
+    stop("at least one tau must be in range of 't'")
+  }
+  #If some taus are out of range of t, drop them
+  if(any(taus > max(data[[quo_name(t)]])) | any(taus < min(data[[quo_name(t)]]))){
+    warn("Some taus were not used because they were outside of range of t")
+    taus <- taus[taus <= max(data[[quo_name(t)]]) & taus >= min(data[[quo_name(t)]])]
+  }
 
   # adds `.post` to formula. Would not be difficult to modify for other interactions
   f <- update(formula, ~. + .post)
   LLs <- c()
-  taus <- {{taus}}
 
   for(i in 1:length(taus)){
     usetau = taus[i]
-    data2 <- mutate({{data}}, .post = ifelse(!!t <= usetau, 0, !!t - usetau))
+    data2 <- mutate(data, .post = ifelse(!!t <= usetau, 0, !!t - usetau))
 
     m0 = try(lm(f, data = data2))
     if(class(m0) != "try-error") LLs[i] = logLik(m0)
@@ -45,7 +53,7 @@ brkpt <- function(data, taus, t, formula){
     tau_win <- median(tau_win)
   }
   # I don't really like that it re-fits the model.  I could have it save them all and only re-fit in the case of a tau tie.
-  data_win <- mutate({{data}}, .post = ifelse(!!t <= tau_win, 0, !!t - tau_win))
+  data_win <- mutate(data, .post = ifelse(!!t <= tau_win, 0, !!t - tau_win))
   m_win <- lm(f, data = data_win)
   return(tibble(tau = tau_win, model = list(m_win)))
 }
--- tests/testthat/test-brkpt.R
+++ tests/testthat/test-brkpt.R
@@ -2,12 +2,30 @@ testbees <- colony_weights[colony_weights$ColonyID=="18"]
 
 test_that("brkpt errors if time variable is missing from formula", {
   expect_error(
-    brkpt(data = testbees, taus = seq(2,8,0.1), t = Round, formula = log(TrueColonyWt_g) ~ 1), "'t=' should specify the time variable in the formula"
+    brkpt(testbees, taus = seq(2,8,0.1), t = Round, formula = log(TrueColonyWt_g) ~ 1),
+    "'t=' should specify the time variable in the formula"
   )
 })
 
 test_that("brkpt works", {
-  expect_is(
-    brkpt(data = testbees, taus = seq(2,8,0.1), t = Round, formula = log(TrueColonyWt_g) ~ Round), "data.frame"
+  expect_s3_class(
+    brkpt(testbees, taus = seq(2,8,0.1), t = Round, formula = log(TrueColonyWt_g) ~ Round),
+    "data.frame"
   )
 })
+
+test_that("brkpt errors if taus don't match t", {
+  expect_error(
+    brkpt(testbees, taus = seq(8.1, 10, 0.1), t = Round, formula = log(TrueColonyWt_g) ~ Round),
+    "at least one tau must be in range of 't'"
+  )
+})
+
+test_that("brkpt uses only taus in range of t", {
+  expect_warning(
+    brkpt(testbees, taus = seq(2, 10, 0.1), t = Round, formula = log(TrueColonyWt_g) ~ Round),
+    "Some taus were not used because they were outside of range of t"
+  )
+})
+
+
