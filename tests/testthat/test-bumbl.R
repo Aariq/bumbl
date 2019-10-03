1b6c8594faba69bc4089976c7d52b28e91c38f41
Aariq <scottericr@gmail.com>
2019-10-03 16:24:46 -0400

    using data subset in tests to make them run faster

--- tests/testthat/test-bumbl.R
+++ tests/testthat/test-bumbl.R
@@ -1,52 +1,57 @@
-testdf <- bombus[bombus$colony != 67, ]
-testdf <-
-  testdf %>%
-  #make fake count data
-  mutate(count = as.integer(mass) - min(as.integer(mass)))
+library(dplyr)
+bombus_sub <-
+  bombus %>%
+  filter(colony %in% c(67, 9, 14, 82, 83, 46, 92)) %>%
+  group_by(colony) %>%
+  mutate(count = as.integer(mass) - min(as.integer(mass))) %>%
+  ungroup()
+
+noerrs <- bombus_sub %>% filter(colony != 67)
+
 test_that("bumbl works", {
   expect_s3_class(
-    bumbl(testdf, colonyID = colony, t = week, formula = log(mass) ~ week),
+    bumbl(noerrs, colonyID = colony, t = week, formula = log(mass) ~ week),
     "data.frame"
   )
 })
 
 test_that("bumbl works with custom taus", {
   expect_s3_class(
-    suppressWarnings(bumbl(testdf, colonyID = colony, taus = seq(3, 18, 0.5), t = week, formula = log(mass) ~ week)),
+    suppressWarnings(bumbl(noerrs, colonyID = colony, taus = seq(3, 18, 0.5), t = week, formula = log(mass) ~ week)),
     "data.frame"
   )
 })
 
 test_that("bumbl drops colonies that produce errors", {
   expect_message({
-    out <- bumbl(bombus, colonyID = colony, t = week, formula = log(mass) ~ week)
+    out <- bumbl(bombus_sub, colonyID = colony, t = week, formula = log(mass) ~ week)
   }, "Warning: More than one equivalent tau found for colonyID '67'. Omitting from results.")
-  expect_equal(nrow(out), length(unique(bombus$colony)) - 1)
+  expect_equal(nrow(out), length(unique(bombus_sub$colony)) - 1)
 })
 
 test_that("bumbl returns NAs for colonies that produce errors when augment = TRUE", {
   expect_message({
-    out <- bumbl(bombus, colonyID = colony, t = week, formula = log(mass) ~ week, augment = TRUE)
+    out <- bumbl(bombus_sub, colonyID = colony, t = week, formula = log(mass) ~ week, augment = TRUE)
   }, "Warning: More than one equivalent tau found for colonyID '67'. Omitting from results.")
-  expect_equal(nrow(bombus), nrow(out))
+  expect_equal(nrow(bombus_sub), nrow(out))
 })
 
 test_that("bumbl works with co-variates", {
-  out <- bumbl(bombus, colonyID = colony, t = week, formula = log(mass) ~ week * cum_floral)
+  out <- bumbl(bombus_sub, colonyID = colony, t = week, formula = log(mass) ~ week * cum_floral)
   expect_identical(colnames(out), c("colony", "tau", "logN0", "logLam", "decay", "logNmax", "cum_floral", "week:cum_floral"))
 })
 
 test_that("no unexpected warnings", {
-  expect_silent(bumbl(testdf, colonyID = colony, t = week, formula = log(mass) ~ week))
-  expect_silent(bumbl(testdf, colonyID = colony, t = week, formula = log(mass) ~ week, augment = TRUE))
+  expect_silent(bumbl(noerrs, colonyID = colony, t = week, formula = log(mass) ~ week))
+  expect_silent(bumbl(noerrs, colonyID = colony, t = week, formula = log(mass) ~ week, augment = TRUE))
 })
 
 test_that("bumbl works with poisson count data", {
   count.out <-
-    bumbl(testdf, colonyID = colony, t = week, formula = count ~ week,
+    bumbl(noerrs, colonyID = colony, t = week, formula = count ~ week,
           family = "poisson")
   count.out.aug <-
-    bumbl(testdf, colonyID = colony, t = week, formula = count ~ week,
+    bumbl(noerrs, colonyID = colony, t = week, formula = count ~ week,
           family = "poisson", augment = TRUE)
   expect_s3_class(count.out, "data.frame")
   expect_s3_class(count.out.aug, c("data.frame", "bumbldf"))
@@ -54,10 +59,10 @@ test_that("bumbl works with poisson count data", {
 
 test_that("bumbl works with overdispersed count data", {
   count.out <-
-    bumbl(testdf, colonyID = colony, t = week, formula = count ~ week,
+    bumbl(noerrs, colonyID = colony, t = week, formula = count ~ week,
           family = "negbin")
   count.out.aug <-
-    bumbl(testdf, colonyID = colony, t = week, formula = count ~ week,
+    bumbl(noerrs, colonyID = colony, t = week, formula = count ~ week,
           family = "negbin", augment = TRUE)
   expect_s3_class(count.out, "data.frame")
   expect_s3_class(count.out.aug, c("data.frame", "bumbldf"))
