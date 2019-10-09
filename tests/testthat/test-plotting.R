86e59e1e1229d93b3dcc7c822c01f47a71112390
Aariq <scottericr@gmail.com>
2019-10-09 14:46:26 -0400

    added tests for plotting, but honestly they don't do anything.  Need to look into the vdiffr package

--- tests/testthat/test-plotting.R
+++ tests/testthat/test-plotting.R
@@ -1,6 +1,22 @@
+library(dplyr)
 colony_subset <- sample(bombus$colony, 10)
-lildf <- bombus[bombus$colony %in% colony_subset, ]
-results <- bumbl(lildf, colonyID = colony, t = week, formula = log(mass) ~ week, augment = TRUE)
+lildf <-
+  bombus %>%
+  filter(colony %in% colony_subset) %>%
+  #make fake count data
+  group_by(colony) %>%
+  mutate(count = as.integer(d.mass)) %>%
+  ungroup()
+
+results <-
+  bumbl(lildf, colonyID = colony, t = week, formula = d.mass ~ week,
+        augment = TRUE)
+results_count <-
+  bumbl(lildf, colonyID = colony, t = week, formula = count ~ week,
+        family = "poisson", augment = TRUE)
+results_overdisp <-
+  bumbl(lildf, colonyID = colony, t = week, formula = count ~ week,
+        family = "negbin", augment = TRUE)
 
 test_that("plotting function displays something?", {
   expect_invisible(bumbl_plot(results))
@@ -10,4 +26,9 @@ test_that("plotting function errors when not a bumbl(augment = TRUE) object", {
   expect_error(bumbl_plot(mtcars),
                "bumbl_plot() only works on dataframes output by bumbl() with augment = TRUE",
                fixed = TRUE)
+})
+
+test_that("plotting works with all families", {
+  expect_invisible(bumbl_plot(results_count))
+  expect_invisible(bumbl_plot(results_overdisp))
 })
